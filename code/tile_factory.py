31535a1ab505a91321c33db16f696ffcc0293a26
Yushan Zheng <yszheng@buaa.edu.cn>
2023-10-24 10:19:39 +0800

    Refine the code for acceleration

--- code/tile_factory.py
+++ code/tile_factory.py
@@ -1,21 +1,38 @@
 import os.path
 import time
+import traceback
+from concurrent.futures import ProcessPoolExecutor
 
-import cv2
-import math
-import threading
 import numpy as np
 from openslide import open_slide
 
 from code.utils import MAGNIFICATION_MAP, MAGNIFICATION_DICT, is_tile_mostly_background
 
 
-def save_tiles(tile_list, tile_names, save_path):
-    for tile, tile_name in zip(tile_list, tile_names):
-        if is_tile_mostly_background(tile):
-            continue
-        y, x = str(tile_name[0]), str(tile_name[1])
-        cv2.imwrite('{}/{}_{}.jpg'.format(save_path, y.zfill(4), x.zfill(4)), tile)
+class GridsCropWorker:
+    def __init__(self, slide_path, width, height, overlap, level, save_path):
+        self.slide_path = slide_path
+        self.width = width
+        self.height = height
+        self.overlap = overlap
+        self.level = level
+
+        self.save_path = save_path
+
+    def crop_tiles(self, grids):
+        slide = open_slide(self.slide_path)
+        crop_num = 0
+        for x, y in grids:
+            try:
+                tile = slide.read_region((x, y), level=self.level, size=(self.width, self.height))
+            except Exception as e:
+                traceback.print_exc()
+                print('Tile in x:{} y:{} crop failed.')
+                continue
+            tile = tile.convert('RGB')
+            tile.save('{}/{}_{}.jpg'.format(self.save_path, str(y).zfill(4), str(x).zfill(4)))
+            crop_num += 1
+        return crop_num
 
 
 def save_level_file(file_name, slide_id, level_tiles_num, magnification, size, tile_size):
@@ -31,17 +48,22 @@ def save_level_file(file_name, slide_id, level_tiles_num, magnification, size, t
 
 
 class TileFactory(object):
-    def __init__(self, slide_path, tile_size, overlap, output_path):
+    def __init__(self, slide_path, tile_size, overlap, output_path, num_workers):
         super(TileFactory).__init__()
-
+        self.slide_path = slide_path
         self.slide_id = ''.join(slide_path.split('\\')[-1].split('.')[:-1])
         self.output_path = os.path.join(output_path, self.slide_id)
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
         self.slide = open_slide(slide_path)
-        self.tile_size = tile_size
-        self.overlap = overlap
-        self.magnification = int(self.slide.properties['openslide.objective-power'])
+        self.tile_size = int(tile_size)
+        self.overlap = int(overlap)
+
+        self.mpp = 10000 / float(self.slide.properties['tiff.XResolution'])
+
+        self.magnification = 40
+        # self.magnification = int(self.slide.properties['openslide.objective-power'])
+        self.num_workers = num_workers
 
     def make_overview(self):
         slide_size = self.slide.level_dimensions[0]
@@ -51,40 +73,39 @@ class TileFactory(object):
         overview_image.save('{}/Overview.jpg'.format(self.output_path))
 
     def make_tiles(self):
-        start = time.time()
-        for level in range(self.slide.level_count):
-            magnification = self.magnification / (2 ** level)
-            try:
-                if magnification in MAGNIFICATION_MAP:
-                    magnification_name = MAGNIFICATION_MAP[magnification]
-                    level_slide_size = self.slide.level_dimensions[level]
+        with ProcessPoolExecutor(max_workers=self.num_workers) as executor:
+            start = time.time()
+            for level in range(self.slide.level_count):
+                magnification = self.magnification / (2 ** level)
+                try:
+                    if magnification in MAGNIFICATION_MAP:
+                        magnification_name = MAGNIFICATION_MAP[magnification]
+                        level_slide_size = self.slide.level_dimensions[level]
 
-                    # saving level information file
-                    level_tiles_num = [int(level_slide_size[0]/self.tile_size), int(level_slide_size[1]/self.tile_size)]
-                    save_level_file('{}/{}.txt'.format(self.output_path, magnification_name), self.slide_id,
-                                    level_tiles_num, magnification, level_slide_size, self.tile_size)
+                        # saving level information file
+                        level_tiles_num = [int(level_slide_size[0]/self.tile_size), int(level_slide_size[1]/self.tile_size)]
+                        save_level_file('{}/{}.txt'.format(self.output_path, magnification_name), self.slide_id,
+                                        level_tiles_num, magnification, level_slide_size, self.tile_size)
 
-                    # saving tile images
-                    slide_image = np.array(self.slide.read_region((0, 0), level, level_slide_size))
-                    slide_image = cv2.cvtColor(slide_image, cv2.COLOR_RGBA2BGR)
+                        crop_step = self.tile_size - self.overlap
+                        xs = np.arange(0, level_slide_size[0] - self.tile_size, crop_step)
+                        ys = np.arange(0, level_slide_size[1] - self.tile_size, crop_step)
 
-                    thread_queue = []
-                    tiles_path = os.path.join(self.output_path, magnification_name)
-                    if not os.path.exists(tiles_path):
-                        os.makedirs(tiles_path)
-                    for y, row_image in enumerate(np.split(slide_image, np.arange(self.tile_size, level_slide_size[0], self.tile_size), axis=0)):
-                        row_tiles = []
-                        tile_pos = []
-                        for x, tile in enumerate(np.split(row_image, np.arange(self.tile_size, level_slide_size[1], self.tile_size), axis=1)):
-                            row_tiles.append(tile)
-                            tile_pos.append((y, x))
+                        x_grids, y_grids = np.meshgrid(xs, ys)
+                        grids = np.stack([x_grids.reshape(-1), y_grids.reshape(-1)], 1)
 
-                        t = threading.Thread(target=save_tiles, args=(row_tiles, tile_pos, tiles_path))
-                        t.start()
-                        thread_queue.append(t)
-            except Exception as e:
-                print('slide: {}\tlevel: {} tiling failed'.format(self.slide_path, magnification))
-                print(e)
-                pass
-        print(time.time() - start)
+                        save_path = os.path.join(self.output_path, magnification_name)
+                        if not os.path.exists(save_path):
+                            os.makedirs(save_path)
+                        grid_crop_worker = GridsCropWorker(self.slide_path, self.tile_size, self.tile_size, self.overlap, level, save_path)
+                        crop_num = list(executor.map(grid_crop_worker.crop_tiles,
+                                                     np.array_split(grids, self.num_workers)))
+                        crop_num = sum(crop_num)
+                        if crop_num != len(grids):
+                            print('slide: {} cropped incompletely')
+                except Exception as e:
+                    traceback.print_exc()
+                    print('slide: {} level: {}x tiling failed'.format(self.slide_path, magnification))
+                    pass
+            print(time.time() - start)
 
--- main.py
+++ main.py
@@ -12,13 +12,16 @@ def get_parser():
                         help='overlap of tiles.')
     parser.add_argument('--slide-file', type=str, default=None,
                         help='path of <has_been_downloaded> file.')
-    parser.add_argument('output-path', type=str, default=None,
+    parser.add_argument('--num-workers', type=str, default=4,
+                        help='num of process for cropping slide.')
+    parser.add_argument('output_path', type=str, default=None,
                         help='path to save the tiles.')
     return parser
 
 
 def main(args):
-    tile_factory = TileFactory(args.slide_file, args.tile_size, args.overlap, output_path=args.output_path)
+    print(args, args.__dict__)
+    tile_factory = TileFactory(args.slide_file, args.tile_size, args.overlap, output_path=args.output_path, num_workers=args.num_workers)
     tile_factory.make_overview()
     tile_factory.make_tiles()
 
